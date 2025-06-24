e07aa058720d78dcfe6c47fceec0e091bbc5774d
NguyenLeMinhAnh <nguyenleminhanh.ai@gmail.com>
2025-06-24 21:15:46 +0700

    Create_file_HTML

--- DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30356d0ffe8d85107e7f5a7f2e3a94c16143dbe8")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
