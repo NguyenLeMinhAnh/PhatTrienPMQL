cb8d298a445d9e740ebd4d3d1b8c76b40a31e4b9
NguyenLeMinhAnh <nguyenleminhanh.ai@gmail.com>
2025-07-02 16:53:28 +0700

    BTH_Controllers_Models_ViewS

--- DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
+++ DemoMVC/obj/Debug/net9.0/DemoMVC.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+56e182c232bf54fdf705d15a3f1955f983547864")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c45ebb6dab40493a1ae68152a5fddb5c6f3b5658")]
 [assembly: System.Reflection.AssemblyProductAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DemoMVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
